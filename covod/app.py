c2cb3ab31fbff9d325a0cd961cc49498ea6ab515
Hax0rm4n <stu210876@mail.uni-kiel.de>
2020-03-20 23:42:57 +0100

    Setup flask-sqlalchemy

--- covod/app.py
+++ covod/app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_env import MetaFlaskEnv
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
 from covod.resources.foo import Foo
 
+class Configuration(metaclass=MetaFlaskEnv):
+    ENV_LOAD_ALL = True
+
 app = Flask(__name__)
+app.config.from_object(Configuration)
+
 api = Api(app)
+db = SQLAlchemy(app)
 
 api.add_resource(Foo, '/foo', '/foo/<string:id>')
 
