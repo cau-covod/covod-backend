a4cd213a889a45f73f4710708b03dbdf319ab210
Hax0rm4n <stu210876@mail.uni-kiel.de>
2020-03-21 14:44:12 +0100

    Mostly complete file upload API

--- covod/resources/lecture.py
+++ covod/resources/lecture.py
@@ -1,28 +1,45 @@
+import uuid
 from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask_cloudy import Storage
+
+from covod.models.models import Lecture, MediaType, Media, db
 
 storage = Storage()
 
 UPLOAD_DIRECTORY='media'
 
+
 class LectureMedia(Resource):
-    def get(self, id=-1):
+    def get(self, id):
         return "Got id " + str(id)
-    def post(self, id=-1):
+
+    def post(self, id):
         file = request.files['file']
-        print(file)
-        upload = storage.upload(file, name=str(id))
-        return {'name':upload.name, 'extension':upload.extension, 'size':upload.size, 'url': upload.url}
+
+        lecture = Lecture.query.filter_by(id=id).first_or_404()
+        media_uuid = uuid.uuid4()
+
+        upload = storage.upload(file, name=str(media_uuid), prefix=f"{lecture.course.uuid}/")
+
+        media = Media(uuid=media_uuid, type=MediaType.AUDIO_VIDEO, lecture=lecture, extension=upload.extension)
+        db.session.add(media)
+        db.session.commit()
+
+        return {'name': upload.name, 'extension': upload.extension, 'size': upload.size, 'url': upload.url}
+
 
 class LecturePDF(Resource):
     def get(self):
         pass
+
     def post(self):
         pass
+
 
 class LectureTimestamps(Resource):
     def get(self):
         pass
+
     def post(self):
         pass
