2e23c176211d64586b2182ce30af1ae44140714f
Hax0rm4n <stu210876@mail.uni-kiel.de>
2020-03-22 18:28:44 +0100

    Try to serve any web-aoo path that isn't empty

--- covod/blueprints/web_app.py
+++ covod/blueprints/web_app.py
@@ -8,7 +8,7 @@ bp = Blueprint("web-app", __name__, url_prefix="/")
 @bp.route("/", defaults={"path": ""})
 @bp.route("/<path:path>")
 def serve(path):
-    if path != "" and os.path.exists("../web-app/" + path):
+    if path != "":  # and os.path.exists("../web-app/" + path):
         return send_from_directory("../web-app", path)
     else:
         return send_from_directory("../web-app", "index.html")
